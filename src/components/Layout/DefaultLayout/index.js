7aa6168fc7deed2873b834d8e4396dbc07738634
vophison2991998@gmail.com <vophison2991998@gmail.com>
2024-03-03 18:42:53 +0700

    update

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -2,14 +2,14 @@ import Header from '../Headder/index.js';
 import Footer from '../Footer/index.js';
 import Sidebar from '../Sidebar/index.js';
 
-function DefaultLayout({ chidren }) {
+function DefaultLayout({ children }) {
     return (
         <div>
             <Header />
             <div className="container">
                 <Sidebar />
 
-                <div className="content">{chidren}</div>
+                <div className="content">{children}</div>
             </div>
             <Footer />
         </div>
